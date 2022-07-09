bf374d2fe5944af5d95f67bb7d9c1a8e257b6631
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-07-10 00:19:50 +0300

    movie view attr columns component

---
+++ frontend/src/components/ListItem.vue
@@ -0,0 +1,20 @@
+<script
+  setup
+  lang="ts"
+>
+const props = defineProps<{
+  title: String | Number;
+}>();
+</script>
+<template>
+  <div>
+    <div>{{ title }}:</div>
+    <span class="font-semibold">
+      <slot />
+    </span>
+  </div>
+</template>
+<style
+  lang="scss"
+  scoped
+></style>
