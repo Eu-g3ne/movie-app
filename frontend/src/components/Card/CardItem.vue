56ca1ef0c63bef82556a6b617df42cbad6b5f732
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-07-10 00:19:19 +0300

    separation on components

--- frontend/src/components/Card/CardItem.vue
+++ frontend/src/components/Card/CardItem.vue
@@ -3,6 +3,7 @@
   lang="ts"
 >
 import type { Movie } from "@/models/Movie";
+import MovieRating from "@/components/movie/MovieRating.vue";
 import { computed } from "vue";
 
 const props = defineProps<{
@@ -14,7 +15,9 @@ const getEpisode = computed(() => {
 });
 </script>
 <template>
-  <div class="relative hover:scale-105 duration-150">
+  <div
+    class="relative hover:scale-105 duration-150 flex flex-col gap-2 justify-start"
+  >
     <router-link :to="movie.slug">
       <figure
         class="relative flex w-full h-[270px] max-w-2xl v-sm:min-w-[172px]"
@@ -27,25 +30,10 @@ const getEpisode = computed(() => {
         <figcaption
           class="absolute text-center w-full h-full p-2 flex flex-row justify-between"
         >
-          <span class="w-14 bg-dark/90 p-2 rounded-xl flex h-min">
-            <span class="w-8">{{ movie.rating }}</span>
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fa"
-              data-icon="star"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 576 512"
-              class="svg-inline--fa fa-star fa-w-18 w-6"
-            >
-              <path
-                fill="currentColor"
-                d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-                class=""
-              ></path>
-            </svg>
-          </span>
+          <MovieRating
+            :rating="movie.rating"
+            readonly
+          />
           <span
             class="w-20 bg-dark/90 p-2 rounded-xl h-min self-end content-center"
           >
@@ -80,7 +68,7 @@ const getEpisode = computed(() => {
         </figcaption>
       </figure>
     </router-link>
-    <div class="pt-3">
+    <div>
       <p class="text-center">{{ movie.title }}</p>
     </div>
   </div>
--- frontend/src/views/MovieView.vue
+++ frontend/src/views/MovieView.vue
@@ -2,44 +2,117 @@
   setup
   lang="ts"
 >
+import { computed } from "vue";
+
+import ListItem from "@/components/ListItem.vue";
+import MovieRating from "@/components/movie/MovieRating.vue";
+import MovieTitle from "@/components/movie/MovieTitle.vue";
+import MovieDescription from "@/components/movie/MovieDescription.vue";
 import { storeToRefs } from "pinia";
 import { useMovieStore } from "@/stores/index";
-import { onMounted } from "vue";
+import { capitalize } from "@/composables/helpers";
 
 const props = defineProps<{
   slug: string;
 }>();
-
-const { movie } = storeToRefs(useMovieStore());
-const { getMovieBySlug } = useMovieStore();
-
-onMounted(() => {
-  // getMovieBySlug(props.slug);
+const { movie, isReadonly } = storeToRefs(useMovieStore());
+const cols = computed<Object>(() => {
+  return {
+    Type: capitalize(movie.value.type),
+    Status: capitalize(movie.value.status),
+    "Stopped on": movie.value.episode,
+    "Total episodes": movie.value.total_episodes,
+  };
 });
 </script>
 <template>
-  <div>
-    <div class="flex flex-col justify-around items-center p-5">
+  <div class="text-sm sm:text-lg p-5">
+    <div class="relative z-0 rounded-xl shadow-2xl min-w-[200px]">
+      <!-- background image -->
       <div
-        class="fixed w-screen h-screen inset-0 -z-1 opacity-5"
-        :style="{
-          background: `url(${movie.image}) center center / cover no-repeat fixed`,
-        }"
+        class="absolute w-full h-full rounded-xl -z-[200] opacity-40 backdrop"
+        :style="{ 'background-image': `url(${movie.image})` }"
       ></div>
-      <div>{{ movie.title }}</div>
-      <div>
-        <p>
-          {{ movie.status }}
-        </p>
-        <p>
-          {{ movie.type }}
-        </p>
+      <!-- background image -->
+      <div class="flex flex-col gap-5 h-auto text-center p-5">
+        <div
+          class="flex flex-col v-sm:flex-row gap-2 justify-between items-start"
+        >
+          <MovieTitle
+            v-model:title="movie.title"
+            :readonly="isReadonly"
+          />
+          <MovieRating
+            v-model:rating="movie.rating"
+            :readonly="isReadonly"
+          />
+          <div class="bg-dark/80 rounded-xl w-16 h-12 whitespace-nowrap">
+            <button
+              class="group w-full h-full"
+              @click.prevent="movie.is_favorite = !movie.is_favorite"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-9 m-auto duration-300 group-hover:duration-200 group-hover:scale-125 group-active:scale-[2]"
+                :fill="movie.is_favorite ? 'white' : 'none'"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                />
+                <path
+                  class="opacity-0 group-hover:opacity-100 duration-500"
+                  v-if="movie.is_favorite"
+                  d="M 10 6 14 8 10 13 14 13 10 18 L 14 20"
+                  stroke="black"
+                  stroke-width="2"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div
+          class="flex flex-col v-sm:flex-row gap-5 items-center justify-between font-normal"
+        >
+          <ListItem
+            v-for="(value, key) in cols"
+            :title="capitalize(key)"
+          >
+            {{ value }}
+          </ListItem>
+        </div>
+        <MovieDescription
+          v-model:description="movie.description"
+          :readonly="isReadonly"
+        />
       </div>
-      <div>Footer</div>
+    </div>
+    <div class="p-5">
+      You want download this movie?
+      <div>go here</div>
+      <div>
+        <h5>amazing list</h5>
+      </div>
     </div>
   </div>
 </template>
 <style
   lang="scss"
   scoped
-></style>
+>
+.backdrop {
+  background-position: center;
+  background-attachment: fixed;
+  background-clip: padding-box;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+h1 {
+  @apply text-3xl;
+}
+</style>
