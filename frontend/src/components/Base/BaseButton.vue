d4eec7e630360e8f474e6fc38c44d946ca059189
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-07-10 00:14:31 +0300

    remove some styles

--- frontend/src/components/Base/BaseButton.vue
+++ frontend/src/components/Base/BaseButton.vue
@@ -16,10 +16,7 @@
   lang="scss"
   scoped
 >
-.adaptive-rounded {
-  @apply first:rounded-t-xl last:rounded-b-xl v-sm:first:rounded-t-none v-sm:last:rounded-b-none v-sm:first:rounded-tl-xl v-sm:first:rounded-bl-xl v-sm:last:rounded-tr-xl v-sm:last:rounded-br-xl;
-}
 .btn {
-  @apply text-white p-4 adaptive-rounded border-2 border-dark border-double duration-150 w-full;
+  @apply text-white duration-150 w-full;
 }
 </style>
