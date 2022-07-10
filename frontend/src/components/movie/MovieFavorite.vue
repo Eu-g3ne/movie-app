80cb5e56dbc818d6142dbd4261d909ab22244d91
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-07-10 23:39:41 +0300

    component separating, grid instead of flex

---
+++ frontend/src/components/movie/MovieFavorite.vue
@@ -0,0 +1,42 @@
+<script
+  setup
+  lang="ts"
+>
+const props = defineProps<{
+  favorite: boolean;
+  readonly?: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:favorite", value: boolean): void;
+}>();
+</script>
+<template>
+  <div class="relative bg-dark/80 h-12 aspect-square rounded-xl group flex">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-8 w-8 m-auto duration-300 group-hover:duration-200 group-hover:scale-125 group-active:scale-[2]"
+      :fill="favorite ? 'white' : 'none'"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      stroke-width="2"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+      />
+      <path
+        class="opacity-0 group-hover:opacity-100 duration-500"
+        v-if="favorite"
+        d="M 10 6 14 8 10 13 14 13 10 18 L 14 20"
+        stroke="black"
+        stroke-width="2"
+      />
+    </svg>
+  </div>
+</template>
+<style
+  lang="scss"
+  scoped
+></style>
--- frontend/src/views/MovieView.vue
+++ frontend/src/views/MovieView.vue
@@ -8,6 +8,7 @@ import ListItem from "@/components/ListItem.vue";
 import MovieRating from "@/components/movie/MovieRating.vue";
 import MovieTitle from "@/components/movie/MovieTitle.vue";
 import MovieDescription from "@/components/movie/MovieDescription.vue";
+import MovieFavorite from "@/components/movie/MovieFavorite.vue";
 import { storeToRefs } from "pinia";
 import { useMovieStore } from "@/stores/index";
 import { capitalize } from "@/composables/helpers";
@@ -27,65 +28,40 @@ const cols = computed<Object>(() => {
 </script>
 <template>
   <div class="text-sm sm:text-lg p-5">
-    <div class="relative z-0 rounded-xl shadow-2xl min-w-[200px]">
+    <div class="relative z-0 rounded-xl shadow-2xl">
       <!-- background image -->
       <div
         class="absolute w-full h-full rounded-xl -z-[200] opacity-40 backdrop"
         :style="{ 'background-image': `url(${movie.image})` }"
       ></div>
       <!-- background image -->
-      <div class="flex flex-col gap-5 h-auto text-center p-5">
+      <div class="grid grid-cols-12 gap-2 h-auto text-center p-5">
+        <MovieTitle
+          class="2xl:col-span-11 sm:col-span-9 col-span-12"
+          v-model:title="movie.title"
+          :readonly="isReadonly"
+        />
         <div
-          class="flex flex-col v-sm:flex-row gap-2 justify-between items-start"
+          class="flex flex-row gap-2 sm:justify-end justify-between 2xl:col-span-1 sm:col-span-3 col-span-12"
         >
-          <MovieTitle
-            v-model:title="movie.title"
-            :readonly="isReadonly"
-          />
           <MovieRating
             v-model:rating="movie.rating"
             :readonly="isReadonly"
           />
-          <div class="bg-dark/80 rounded-xl w-16 h-12 whitespace-nowrap">
-            <button
-              class="group w-full h-full"
-              @click.prevent="movie.is_favorite = !movie.is_favorite"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-9 m-auto duration-300 group-hover:duration-200 group-hover:scale-125 group-active:scale-[2]"
-                :fill="movie.is_favorite ? 'white' : 'none'"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-                <path
-                  class="opacity-0 group-hover:opacity-100 duration-500"
-                  v-if="movie.is_favorite"
-                  d="M 10 6 14 8 10 13 14 13 10 18 L 14 20"
-                  stroke="black"
-                  stroke-width="2"
-                />
-              </svg>
-            </button>
-          </div>
+          <MovieFavorite
+            v-model:favorite="movie.is_favorite"
+            :readonly="isReadonly"
+          />
         </div>
-        <div
-          class="flex flex-col v-sm:flex-row gap-5 items-center justify-between font-normal"
+        <ListItem
+          class="v-sm:col-span-3 col-span-12"
+          v-for="(value, key) in cols"
+          :value="capitalize(key)"
         >
-          <ListItem
-            v-for="(value, key) in cols"
-            :title="capitalize(key)"
-          >
-            {{ value }}
-          </ListItem>
-        </div>
+          {{ value }}
+        </ListItem>
         <MovieDescription
+          class="col-span-12"
           v-model:description="movie.description"
           :readonly="isReadonly"
         />
