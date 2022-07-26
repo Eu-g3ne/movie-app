ab64fc99554c66f8a1e51d95bd813766e50777d6
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-07-27 00:49:37 +0300

    divided string/number info components

--- frontend/src/components/movie/MovieEpisode.vue
+++ frontend/src/components/movie/MovieEpisode.vue
@@ -7,7 +7,7 @@ import { computed } from "vue";
 const props = withDefaults(
   defineProps<{
     title: string;
-    value: string | number;
+    value: number;
     readonly?: boolean;
     min?: number;
     max?: number;
@@ -21,19 +21,13 @@ const emit = defineEmits<{
   (e: "update:value", value: string | number): void;
 }>();
 
-const inputType = computed<string>(() => {
-  return typeof props.value === "string" ? "text" : "number";
-});
-
 function updateValue(e: WheelEvent): void {
-  if (typeof props.value === "number") {
-    if (e.deltaY < 0 && validate()) {
-      emit("update:value", props.value + 1);
-    } else if (e.deltaY > 0 && props.min < props.value) {
-      emit("update:value", props.value - 1);
-    }
-    e.preventDefault();
+  if (e.deltaY < 0 && validate()) {
+    emit("update:value", props.value + 1);
+  } else if (e.deltaY > 0 && props.min < props.value) {
+    emit("update:value", props.value - 1);
   }
+  e.preventDefault();
 }
 
 function validate(): boolean {
@@ -68,12 +62,12 @@ const val = computed({
       >
       <input
         v-else
-        :type="inputType"
+        type="number"
         maxlength="128"
         :max="max"
         :min="min"
         v-model="val"
-        v-on="{ wheel: inputType === 'number' ? updateValue : null }"
+        @wheel="updateValue"
       />
     </Transition>
   </div>
@@ -84,7 +78,7 @@ const val = computed({
 >
 input,
 span {
-  @apply bg-dark/0 font-normal sm:font-semibold ml-0 mr-auto w-full tracking-[2px];
+  @apply bg-dark/0 font-normal sm:font-semibold ml-0 mr-auto w-full tracking-[2px] h-12;
 }
 input {
   @include editable-border;
---
+++ frontend/src/components/movie/MovieSelect.vue
@@ -0,0 +1,79 @@
+<script
+  setup
+  lang="ts"
+>
+import { computed } from "vue";
+
+const props = defineProps<{
+  title: string;
+  value: string;
+  values: Array<string>;
+  readonly?: boolean;
+}>();
+const emit = defineEmits<{
+  (e: "update:value", value: string): void;
+}>();
+
+const val = computed({
+  get() {
+    return props.value;
+  },
+  set(val) {
+    emit("update:value", val);
+  },
+});
+</script>
+<template>
+  <div>
+    <div>{{ title }}:</div>
+    <Transition
+      name="fade"
+      mode="out-in"
+      :duration="300"
+    >
+      <span
+        v-if="readonly"
+        :title="value"
+        >{{ value }}</span
+      >
+      <select
+        v-else
+        name="select"
+        id="select"
+        v-model="val"
+      >
+        <optgroup :label="title">
+          <option
+            v-for="(val, index) in values"
+            :value="val"
+            :key="index + val"
+          >
+            {{ val }}
+          </option>
+        </optgroup>
+      </select>
+    </Transition>
+  </div>
+</template>
+<style
+  lang="scss"
+  scoped
+>
+select {
+  @apply bg-light/0 w-full rounded-lg p-2 cursor-pointer sm:font-semibold leading-6 border-white border-2 border-solid tracking-[2px] h-12;
+}
+
+option,
+optgroup {
+  @apply bg-dark cursor-pointer sm:font-semibold;
+}
+select,
+option,
+optgroup {
+  @apply tracking-[2px];
+}
+
+span {
+  @apply bg-dark/0 font-normal sm:font-semibold ml-0 mr-auto w-full tracking-[2px] p-2;
+}
+</style>
