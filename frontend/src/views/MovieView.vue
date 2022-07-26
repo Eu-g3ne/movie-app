27a02cd7dafb759ce2536737a8b4f861c6f7c9ef
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-07-27 01:03:27 +0300

    change layout, save button(working)

--- frontend/src/views/MovieView.vue
+++ frontend/src/views/MovieView.vue
@@ -2,36 +2,37 @@
   setup
   lang="ts"
 >
-import { computed } from "vue";
-
-import MovieInfo from "@/components/movie/MovieInfo.vue";
-import MovieRating from "@/components/movie/MovieRating.vue";
 import MovieTitle from "@/components/movie/MovieTitle.vue";
 import MovieDescription from "@/components/movie/MovieDescription.vue";
 import MovieFavorite from "@/components/movie/MovieFavorite.vue";
+import MovieEpisode from "@/components/movie/MovieEpisode.vue";
+import MovieRating from "@/components/movie/MovieRating.vue";
 import MovieTags from "@/components/movie/MovieTags.vue";
+import MovieSelect from "@/components/movie/MovieSelect.vue";
 import BaseButton from "@/components/base/BaseButton.vue";
+import FileInput from "@/components/FileInput.vue";
 import { storeToRefs } from "pinia";
 import { useMovieStore } from "@/stores/index";
+import type { Movie } from "@/models/Movie";
 
 const props = defineProps<{
   slug: string;
 }>();
-const { movie, isReadonly } = storeToRefs(useMovieStore());
+const { updateMovie } = useMovieStore();
+const { movie, isReadonly, editMode } = storeToRefs(useMovieStore());
+
+function saveChanges(movie: Movie) {
+  updateMovie(movie, props.slug);
+  editMode.value = false;
+}
 </script>
 <template>
   <div class="text-sm sm:text-lg p-5">
     <div class="relative z-0 rounded-xl shadow-2xl">
       <!-- background image -->
       <div
-        v-if="isReadonly"
-        class="absolute w-full h-full rounded-xl -z-[200] opacity-40 backdrop"
-        :style="{ 'background-image': `url(${movie.image})` }"
-      ></div>
-      <div
-        v-else
-        class="absolute w-full h-full rounded-xl -z-[200] opacity-40 backdrop"
-        :style="{ 'background-image': `url(${movie.image})` }"
+        class="absolute w-full h-full rounded-xl -z-[200] opacity-20 backdrop"
+        :style="{ 'background-image': `url(${movie.image.background})` }"
       ></div>
       <!-- background image -->
       <div
@@ -43,7 +44,7 @@ const { movie, isReadonly } = storeToRefs(useMovieStore());
           :readonly="isReadonly"
         />
         <div
-          class="flex flex-row gap-2 sm:justify-end justify-between 2xl:col-span-1 sm:col-span-3 col-span-12"
+          class="flex flex-row gap-2 justify-between sm:justify-end items-center 2xl:col-span-1 sm:col-span-3 col-span-12"
         >
           <MovieRating
             v-model:rating="movie.rating"
@@ -54,26 +55,28 @@ const { movie, isReadonly } = storeToRefs(useMovieStore());
             :readonly="isReadonly"
           />
         </div>
-        <MovieInfo
+        <MovieSelect
           class="v-sm:col-span-3 col-span-12 self-end"
           title="Type"
           v-model:value="movie.type"
+          :values="['anime', 'film', 'serial']"
           :readonly="isReadonly"
         />
-        <MovieInfo
+        <MovieSelect
           class="v-sm:col-span-3 col-span-12 self-end"
           title="Status"
           v-model:value="movie.status"
+          :values="['desired', 'finished', 'ongoing']"
           :readonly="isReadonly"
         />
-        <MovieInfo
+        <MovieEpisode
           class="v-sm:col-span-3 col-span-12 self-end"
           title="Episode"
           v-model:value="movie.episode"
           :readonly="isReadonly"
           :max="movie.total_episodes"
         />
-        <MovieInfo
+        <MovieEpisode
           class="v-sm:col-span-3 col-span-12 self-end"
           title="Total episodes"
           v-model:value="movie.total_episodes"
@@ -85,22 +88,53 @@ const { movie, isReadonly } = storeToRefs(useMovieStore());
           v-model:description="movie.description"
           :readonly="isReadonly"
         />
-        <div class="col-span-6">
+
+        <div class="col-span-12 sm:col-span-5 self-end">
           <MovieTags
             v-model:tags="movie.categories"
             :readonly="isReadonly"
           />
         </div>
-        <div class="col-span-6 justify-self-end self-end">
-          <BaseButton class="bg-dark p-3 rounded-xl">Save changes</BaseButton>
-        </div>
-      </div>
-    </div>
-    <div class="p-5">
-      You want download this movie?
-      <div>go here</div>
-      <div>
-        <h5>amazing list</h5>
+        <Transition
+          name="fade"
+          mode="out-in"
+          :duration="300"
+        >
+          <template v-if="!isReadonly">
+            <div
+              class="col-span-12 sm:col-span-5 justify-self-center self-center w-full h-64 v-sm:h-full v-sm:min-h-[8rem] max-h-[16rem] flex flex-col v-sm:flex-row justify-center gap-3"
+            >
+              <FileInput
+                class="flex-1"
+                caption="Poster"
+                v-model:image="movie.image.poster"
+              />
+
+              <FileInput
+                class="flex-1"
+                caption="Background"
+                v-model:image="movie.image.background"
+              />
+            </div>
+          </template>
+        </Transition>
+        <Transition
+          name="fade"
+          mode="out-in"
+          :duration="300"
+        >
+          <div
+            v-if="!isReadonly"
+            class="col-span-12 justify-self-center sm:col-start-11 sm:col-end-13 sm:place-self-end"
+          >
+            <BaseButton
+              class="bg-dark p-3 rounded-xl duration-300 drop-shadow-sm hover:drop-shadow-xl text-ellipsis"
+              @click.prevent="saveChanges(movie)"
+            >
+              Save changes
+            </BaseButton>
+          </div>
+        </Transition>
       </div>
     </div>
   </div>
