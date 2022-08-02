ad0b7212968187a3917f0796fc1e0b7ff093a0d4
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-02 21:05:32 +0300

    changed db for heroku

--- backend/config/database.php
+++ backend/config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('postgres://ahqwdgupusoeiy:d972f7e55da400ef4161afde39eced67abc85d572c1e3ac2967eb20c1cdac654@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/db93f1uaom87kg');
+
 return [
 
   /*
@@ -15,7 +17,7 @@ return [
     |
     */
 
-  'default' => env('DB_CONNECTION', 'mysql'),
+  'default' => env('DB_CONNECTION', 'pgsql'),
 
   /*
     |--------------------------------------------------------------------------
@@ -65,17 +67,15 @@ return [
 
     'pgsql' => [
       'driver' => 'pgsql',
-      'url' => env('DATABASE_URL'),
-      'host' => env('DB_HOST', '127.0.0.1'),
-      'port' => env('DB_PORT', '5432'),
-      'database' => env('DB_DATABASE', 'forge'),
-      'username' => env('DB_USERNAME', 'forge'),
-      'password' => env('DB_PASSWORD', ''),
+      'host' => $DATABASE_URL["host"],
+      'port' => $DATABASE_URL["port"],
+      'database' => ltrim($DATABASE_URL["path"], "/"),
+      'username' => $DATABASE_URL["user"],
+      'password' => $DATABASE_URL["pass"],
       'charset' => 'utf8',
       'prefix' => '',
-      'prefix_indexes' => true,
-      'search_path' => 'public',
-      'sslmode' => 'prefer',
+      'schema' => 'public',
+      'sslmode' => 'require',
     ],
 
     'sqlsrv' => [
