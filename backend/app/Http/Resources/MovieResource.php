f707c5661fb2f410f70b657d5e95cdb5a89eaa2e
Eu-g3ne <evgeniy.kotov77@gmail.com>
2022-08-07 01:02:59 +0300

    fix image link

--- backend/app/Http/Resources/MovieResource.php
+++ backend/app/Http/Resources/MovieResource.php
@@ -32,8 +32,8 @@ class MovieResource extends JsonResource
       'image' => [
         // 'poster' => $this->image->poster ?: null,
         // 'background' => $this->image->background ?: null,
-        'poster' => getenv('FILESYSTEM_DISK') === 's3' ? Storage::temporaryUrl($this->image->poster, now()->addMinutes(10)) : $this->image->poster,
-        'background' => getenv('FILESYSTEM_DISK') === 's3' ? Storage::temporaryUrl($this->image->background, now()->addMinutes(10)) : $this->image->background,
+        'poster' => getenv('FILESYSTEM_DISK') === 'local' ? $this->image->poster : Storage::temporaryUrl($this->image->poster, now()->addMinutes(10)),
+        'background' => getenv('FILESYSTEM_DISK') === 'local' ? $this->image->background : Storage::temporaryUrl($this->image->background, now()->addMinutes(10)),
       ],
       'categories' => $this->categories->pluck('name'),
     ];
